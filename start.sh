e9db3fef99af2887b483b51ea8835d86166d82ad
wangy <wysclc@163.com>
2026-08-27 11:14:09 +0800

    update

--- start.sh
+++ start.sh
@@ -5,8 +5,7 @@ bin=influxdb-relay
 GOOS=linux GOARCH=amd64 go build -o $bin main.go
 
 rsync -avx --progress $bin hk01:/usr/local/bin/
-rsync -avx --progress influxdb-relay.conf hk01:/etc/influxdb-relay/
+rsync -avx --progress influxdb-relay.toml hk01:/etc/influxdb-relay/
 rsync -avx --progress ./scripts/influxdb-relay.service hk01:/etc/systemd/system/
-ssh hk01 "systemctl stop influxdb-relay && \
- systemctl start influxdb-relay && \
+ssh hk01 "systemctl restart influxdb-relay && \
  systemctl status influxdb-relay"
