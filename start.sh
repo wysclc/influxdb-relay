47fef11eb4cff4e2b309d0a5bdc4153abd4614fb
wangy <wysclc@163.com>
2026-09-22 15:57:22 +0800

    update

--- start.sh
+++ start.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 
 bin=influxdb-relay
-#hosts="jp jp2"
-hosts="jp"
 
 GOOS=linux GOARCH=amd64 go build -o $bin main.go
 
-for host in $hosts;do
-  rsync -avxz --progress $bin "$host":/usr/local/bin/
-  rsync -avx --progress influxdb-relay.toml "$host":/usr/local/etc/influxdb-relay/conf.toml
-  rsync -avx --progress ./scripts/influxdb-relay.service "$host":/etc/systemd/system/
-  ssh "$host" "systemctl restart influxdb-relay && \
-   systemctl status influxdb-relay"
-done
+# vultr-jp
+  rsync -axz --progress $bin jp:/usr/local/bin/
+  rsync -ax --progress influxdb-relay-vultrjp.toml jp:/usr/local/etc/influxdb-relay/conf.toml
+  rsync -ax --progress ./scripts/influxdb-relay.service jp:/etc/systemd/system/
+  ssh jp "systemctl restart influxdb-relay && systemctl status influxdb-relay"
+
+# aly-jp
+#  rsync -axz --progress $bin jp2:/usr/local/bin/
+#  rsync -ax --progress influxdb-relay-alyjp.toml jp2:/usr/local/etc/influxdb-relay/conf.toml
+#  rsync -ax --progress ./scripts/influxdb-relay.service jp2:/etc/systemd/system/
+#  ssh jp2 "systemctl restart influxdb-relay &&  systemctl status influxdb-relay"
