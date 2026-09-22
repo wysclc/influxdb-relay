0ba45abddb706effc53d3b2ff1fac1b58c213af5
wangy <wysclc@163.com>
2026-09-22 15:57:42 +0800

    update start.sh

--- start.sh
+++ start.sh
@@ -10,8 +10,8 @@ GOOS=linux GOARCH=amd64 go build -o $bin main.go
   rsync -ax --progress ./scripts/influxdb-relay.service jp:/etc/systemd/system/
   ssh jp "systemctl restart influxdb-relay && systemctl status influxdb-relay"
 
-# aly-jp
-#  rsync -axz --progress $bin jp2:/usr/local/bin/
-#  rsync -ax --progress influxdb-relay-alyjp.toml jp2:/usr/local/etc/influxdb-relay/conf.toml
-#  rsync -ax --progress ./scripts/influxdb-relay.service jp2:/etc/systemd/system/
-#  ssh jp2 "systemctl restart influxdb-relay &&  systemctl status influxdb-relay"
+ aly-jp
+  rsync -axz --progress $bin jp2:/usr/local/bin/
+  rsync -ax --progress influxdb-relay-alyjp.toml jp2:/usr/local/etc/influxdb-relay/conf.toml
+  rsync -ax --progress ./scripts/influxdb-relay.service jp2:/etc/systemd/system/
+  ssh jp2 "systemctl restart influxdb-relay &&  systemctl status influxdb-relay"
